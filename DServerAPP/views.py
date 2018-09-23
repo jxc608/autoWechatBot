aae110ad868a1e6bd1eb623572626354acda055a
Yeteng <987835616@qq.com>
2018-09-23 17:36:28 +0800

    登录、注册的事件处理，后台view方法渲染，登录成功后的二维码初步显示

--- DServerAPP/views.py
+++ DServerAPP/views.py
@@ -13,6 +13,17 @@ def registerPage(request):
 
 def register(request):
     username=request.POST.get('username','')
+    password=request.POST.get('password','')
+    cpassword=request.POST.get('cpassword','')
+
+    if(username == "") {
+        return render(request, 'DServerAPP/index.html', {"account": 1, "acct":"账号为空" })
+    } else if(password == "") {
+        return render(request, 'DServerAPP/index.html', {"pwd": 1, "pwct": "密码为空"})
+    } else if(1 == 0) {
+        return render(request, 'DServerAPP/index.html', {"pwd": 1, "pwct": "两次密码不一致"})
+    }
+
     try:
         club = Clubs.objects.get(user_name=username)
         return HttpResponse(messageType.createMessage('success', messageType.USER_NAME_ALREADY_USED, 'the userName has already been used'))
@@ -25,7 +36,15 @@ def register(request):
 def login_password(request):
     username=request.POST.get('username','')
     password=request.POST.get('password','')
-    return HttpResponse('username='+username+"&password="+password)
+    if(username == "") {
+        return render(request, 'DServerAPP/index.html', {"account": 1, "acct":"账号为空" })
+    } else if(password == "") {
+        return render(request, 'DServerAPP/index.html', {"pwd": 1, "pwct": "密码为空"})
+    } else if(1 == 0) {
+        return render(request, 'DServerAPP/index.html', {"pwd": 1, "pwct": "密码错误"})
+    }
+    #登录成功之后，渲染页面
+    return render(request, 'DServerAPP/login_succ.html', {"codeSrc": "https://gss0.bdstatic.com/94o3dSag_xI4khGkpoWK1HF6hhy/baike/s%3D220/sign=660c358c4aed2e73f8e9812eb700a16d/08f790529822720e5c8538f27bcb0a46f21fab6b.jpg"})
 
 def login_smscode(request):
     username=request.POST.get('username','')
