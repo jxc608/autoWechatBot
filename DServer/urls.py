e70588830c951f24003f00fc0b1598ae1dce8caa
Yeteng <987835616@qq.com>
2018-12-07 11:42:28 +0800

    解决admin具体表格项，点不进去的问题

--- DServer/urls.py
+++ DServer/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'^grappelli/', include('grappelli.urls')),
     path('', include('DServerAPP.urls')),
     #path(r'^DServerAPP/', 'DServerAPP.urls'),
 ]
