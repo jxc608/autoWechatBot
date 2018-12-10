83166703625ac7645e639cfe43716a6c7d92cbd3
Yeteng <987835616@qq.com>
2018-12-10 16:47:21 +0800

    admin完善

--- DServerAPP/admin.py
+++ DServerAPP/admin.py
@@ -1,28 +1,81 @@
 from django.contrib import admin
 import time
+import datetime
+import json
 # Register your models here.
 from .models import *
+from daterange_filter.filter import DateRangeForm, clean_input_prefix, FILTER_PREFIX, DateRangeFilter
+
+
+class DateStampRangeFilter(DateRangeFilter):
+    template = 'daterange_filter/filter.html'
+
+    def __init__(self, field, request, params, model, model_admin, field_path):
+        self.lookup_kwarg_since = '%s%s__gte' % (FILTER_PREFIX, field_path)
+        self.lookup_kwarg_upto = '%s%s__lte' % (FILTER_PREFIX, field_path)
+        # 转换为时间戳
+        if params.get(self.lookup_kwarg_since):
+            self.lookup_kwarg_since_value = int(time.mktime(time.strptime(params[self.lookup_kwarg_since], "%Y-%m-%d")))
+        if params.get(self.lookup_kwarg_upto):
+            self.lookup_kwarg_upto_value = int(time.mktime(time.strptime(params[self.lookup_kwarg_upto], "%Y-%m-%d")))
+
+        super(DateRangeFilter, self).__init__(
+            field, request, params, model, model_admin, field_path)
+        self.form = self.get_form(request)
+
+    def choices(self, cl):
+        """
+        Pop the original parameters, and return the date filter & other filter
+        parameters.
+        """
+
+        cl.params.pop(self.lookup_kwarg_since, None)
+        cl.params.pop(self.lookup_kwarg_upto, None)
+        return ({
+                    'get_query': cl.params,
+                },)
+
+    def queryset(self, request, queryset):
+        if hasattr(self, "lookup_kwarg_since_value"):
+            print(self.lookup_kwarg_since_value)
+            queryset = queryset.filter(create_time__gte=self.lookup_kwarg_since_value)
+        if hasattr(self, "lookup_kwarg_upto_value"):
+            print(self.lookup_kwarg_upto)
+            print(self.lookup_kwarg_upto_value)
+            queryset = queryset.filter(create_time__lte=self.lookup_kwarg_upto_value)
+        return queryset
+
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ["id", "question_text", "pub_date"]
+    search_fields = ["question_text"]
+    list_filter = [("pub_date", DateRangeFilter)]
+
 
 @admin.register(Choice)
 class ChoiceAdmin(admin.ModelAdmin):
     list_display = ["id", "question", "choice_text", "votes"]
+    search_fields = ["question__question_text", "choice_text"]
 
 @admin.register(Clubs)
 class ClubsAdmin(admin.ModelAdmin):
-    list_display = ["uuid", "user_name", "password", "expired_time", "cost_mode", "cost_param", "profit", "refresh_time"]
+    list_display = ["uuid", "user_name", "password", "show_expired_time", "cost_mode", "cost_param", "profit", "refresh_time"]
     search_fields = ["uuid", "user_name", "cost_param"]
     list_filter = ["cost_mode"]
+    ordering = ["-expired_time"]
+    readonly_fields = ["expired_time", "profit", "refresh_time"]
+
+    def show_expired_time(self, obj):
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(obj.expired_time))
+    show_expired_time.short_description = "过期时间"
 
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
-    list_display = ["id", "club", "wechat_id", "wechat_uuid", "wechat_nick_name", "nick_name", "current_score", "history_profit", "history_cost", "introducer",
-                    "today_hoster_number", "score_limit", "score_limit_desc", "is_del", "is_bind"]
+    list_display = ["id", "club", "wechat_id", "wechat_uuid", "wechat_nick_name", "nick_name", "current_score", "history_profit", "history_cost", "introducer", "today_hoster_number", "score_limit", "score_limit_desc", "is_del", "is_bind"]
     search_fields = ["club__user_name", "wechat_uuid", "wechat_nick_name", "nick_name", "score_limit_desc"]
     list_filter = ["is_del", "is_bind"]
+    readonly_fields = ["club", "wechat_id", "wechat_uuid", "wechat_nick_name", "nick_name", "current_score", "history_profit", "history_cost", "introducer", "today_hoster_number", "is_del", "is_bind"]
 
 # @admin.register(PlayerClearCost)
 # class PlayerClearCost(admin.ModelAdmin):
@@ -31,18 +84,23 @@ class PlayerAdmin(admin.ModelAdmin):
 @admin.register(GameID)
 class GameIdAdmin(admin.ModelAdmin):
     list_display = ["id", "player", "club", "game_nick_name", "gameid"]
-    search_fields = ["player", "club", "game_nick_name", "gameid"]
+    search_fields = ["player__nick_name", "club__user_name", "game_nick_name", "gameid"]
+    readonly_fields = ["player", "club", "game_nick_name", "gameid"]
 
 @admin.register(Score)
 class ScoreAdmin(admin.ModelAdmin):
     list_display = ["id", "player", "score", "cost", "is_host", "create_time", "room_id", "refresh_time"]
     search_fields = ["player__nick_name", "room_id"]
     list_filter = ["is_host"]
+    readonly_fields = ["player", "score", "cost", "is_host", "create_time", "room_id", "refresh_time"]
+    ordering = ["refresh_time"]
 
 @admin.register(HistoryGame)
 class HistoryGameAdmin(admin.ModelAdmin):
-    list_display = ["id", "club", "room_id", "hoster_name", "hoster_id", "round_number", "start_time", "player_data", "create_time", "cost", "score", "refresh_time"]
+    list_display = ["id", "club", "room_id", "hoster_name", "hoster_id", "round_number", "start_time", "create_time", "cost", "score", "refresh_time"]
     search_fields = ["club__user_name", "hoster_name", "hoster_id"]
+    readonly_fields = ["club", "room_id", "hoster_name", "hoster_id", "round_number", "start_time", "player_data", "create_time", "cost", "score", "refresh_time"]
+    ordering = ["-refresh_time"]
 
 # @admin.register(HistoryGameClearCost)
 # class HistoryGameClearCostAdmin(admin.ModelAdmin):
@@ -50,24 +108,52 @@ class HistoryGameAdmin(admin.ModelAdmin):
 
 @admin.register(Cdkey)
 class CdkeyAdmin(admin.ModelAdmin):
-    list_display = ["cdkey", "key_type", "status", "create_time"]
+    list_display = ["cdkey", "key_type", "status", "show_create_time"]
     search_fields = ["cdkey"]
-    list_filter = ["key_type", "status"]
+    list_filter = ["key_type", "status", ("create_time", DateStampRangeFilter)]
+    readonly_fields = ["cdkey", "key_type", "status", "create_time"]
+
+    ordering = ("-create_time",)
+
+    def show_create_time(self, obj):
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(obj.create_time))
+    show_create_time.short_description = "创建时间"
 
 @admin.register(WrongImage)
 class WrongImageAdmin(admin.ModelAdmin):
-    list_display = ["id", "club_name", "image", "create_time"]
+    list_display = ["id", "club_name", "image", "show_create_time"]
     search_fields = ["club_name"]
+    readonly_fields = ["club_name", "image", "create_time"]
+
+    def show_create_time(self, obj):
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(obj.create_time))
+
+    show_create_time.short_description = "创建时间"
 
 @admin.register(ScoreChange)
 class ScoreChangeAdmin(admin.ModelAdmin):
-    list_display = ["id", "player", "score", "agent", "ip", "create_time"]
-    search_fields = ["player__nick_name", "agent", "ip"]
+    list_display = ["id", "player", "score", "agent", "ip", "show_create_time"]
+    search_fields = ["player__nick_name", "ip"]
+    list_filter = ["agent"]
+    readonly_fields = ["id", "player", "score", "agent", "ip", "create_time"]
+    ordering = ("-create_time",)
+
+    def show_create_time(self, obj):
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(obj.create_time))
+
+    show_create_time.short_description = "创建时间"
 
 @admin.register(Manager)
 class ManagerAdmin(admin.ModelAdmin):
-    list_display = ["id", "club", "wechat_nick_name", "nick_name", "create_time"]
+    list_display = ["id", "club", "wechat_nick_name", "nick_name", "show_create_time"]
     search_fields = ["club__user_name", "wechat_nick_name", "nick_name"]
 
+    readonly_fields = ["create_time"]
+
+
+    def show_create_time(self, obj):
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(obj.create_time))
+
+    show_create_time.short_description = "创建时间"
     # def get_time(self):
     #     return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(self.create_time))
--- DServerAPP/models.py
+++ DServerAPP/models.py
@@ -2,111 +2,179 @@
 from django.db import models
 import uuid
 
+USE_STATUS = [(0,"未使用"), (1, "已使用")]
+CDKEY_TYPE = [(1, "一天"), (2, "一周"), (3, "一个月")]
+COST_MODE = [(0, '前x名固定或百分比'), (1, '前x名范围'), (2, '所有固定')]
+IS_HOST = [(0, '是'), (1, '不是')]
+YES_NO_GENERATE = [(0, '是'), (1, '否')]
+
 # Create your models here.
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    question_text = models.CharField(max_length=200, verbose_name="问题内容")
+    pub_date = models.DateTimeField(verbose_name="发布时间")
+
+    def __str__(self):
+        return self.question_text
+
+    class Meta:
+        verbose_name = "问题"
+        verbose_name_plural = verbose_name
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name="问题")
+    choice_text = models.CharField(max_length=200, verbose_name="选择")
     votes = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.choice_text
+
+    class Meta:
+        verbose_name = "选择"
+        verbose_name_plural = verbose_name
+
 class Clubs(models.Model):
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user_name = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    expired_time = models.DecimalField(max_digits=19, decimal_places=6)
-    cost_mode = models.IntegerField(default=0)#管理费模式 固定 还是 百分比
-    cost_param = models.CharField(max_length=1000, default='none')
-    profit = models.IntegerField(default=0)
-    refresh_time = models.IntegerField(default=0)
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name='uuid')
+    user_name = models.CharField(max_length=20, verbose_name='名称')
+    password = models.CharField(max_length=20, verbose_name='密码')
+    expired_time = models.DecimalField(max_digits=19, decimal_places=6, verbose_name='过期时间')
+    cost_mode = models.IntegerField(default=0, choices=COST_MODE, verbose_name='管理模式')#管理费模式 固定 还是 百分比
+    cost_param = models.CharField(max_length=1000, default='none', verbose_name='管理费参数')
+    profit = models.IntegerField(default=0, verbose_name='利润')
+    refresh_time = models.IntegerField(default=0, verbose_name='使用cdkey次数')
 
     def __str__(self):
         return self.user_name
 
+    class Meta:
+        verbose_name = "俱乐部"
+        verbose_name_plural = verbose_name
+
 class Player(models.Model):
-    club = models.ForeignKey(Clubs, on_delete=models.CASCADE)
-    wechat_id = models.CharField(max_length=200)
-    wechat_uuid = models.CharField(max_length=200)
-    wechat_nick_name = models.CharField(max_length=200)
-    nick_name = models.CharField(max_length=200)
-    current_score = models.IntegerField(default=0)
-    history_profit = models.IntegerField(default=0)
-    history_cost = models.IntegerField(default=0)
-    introducer = models.CharField(max_length=200, default='none')
-    today_hoster_number = models.IntegerField(default=0)
-    score_limit = models.IntegerField(default=0)
-    score_limit_desc = models.CharField(max_length=500)
-    is_del = models.IntegerField(default=0)
-    is_bind = models.IntegerField(default=0)
+    club = models.ForeignKey(Clubs, on_delete=models.CASCADE, verbose_name="俱乐部")
+    wechat_id = models.CharField(max_length=200, verbose_name="微信ID")
+    wechat_uuid = models.CharField(max_length=200, verbose_name="微信UUID")
+    wechat_nick_name = models.CharField(max_length=200, verbose_name="微信昵称")
+    nick_name = models.CharField(max_length=200, verbose_name="游戏中昵称")
+    current_score = models.IntegerField(default=0, verbose_name="当前分数")
+    history_profit = models.IntegerField(default=0, verbose_name="总利润")
+    history_cost = models.IntegerField(default=0, verbose_name="总消费")
+    introducer = models.CharField(max_length=200, default='none', verbose_name="介绍人")
+    today_hoster_number = models.IntegerField(default=0, verbose_name="今日开房次数")
+    score_limit = models.IntegerField(default=0, verbose_name="分数上限")
+    score_limit_desc = models.CharField(max_length=500, verbose_name="授信")
+    is_del = models.IntegerField(default=0, choices=YES_NO_GENERATE, verbose_name="是否删除")
+    is_bind = models.IntegerField(default=0, choices=YES_NO_GENERATE, verbose_name="是否绑定")
 
     def __str__(self):
         return  self.nick_name
+
+    class Meta:
+        verbose_name = "玩家信息"
+        verbose_name_plural = verbose_name
 
 class PlayerClearCost(models.Model):
     player_id = models.IntegerField(default=0)
     history_cost = models.IntegerField(default=0)
     create_time = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name = "玩家管理费清理记录"
+        verbose_name_plural = verbose_name
+
 class GameID(models.Model):
-    player = models.ForeignKey(Player, on_delete=models.CASCADE)    
-    club = models.ForeignKey(Clubs, on_delete=models.CASCADE)
-    game_nick_name = models.CharField(max_length=200)
-    gameid = models.CharField(max_length=20)
+    #一个俱乐部中的某用户，出现在了某个game中
+    player = models.ForeignKey(Player, on_delete=models.CASCADE, verbose_name="用户")
+    club = models.ForeignKey(Clubs, on_delete=models.CASCADE, verbose_name="俱乐部")
+    game_nick_name = models.CharField(max_length=200, verbose_name="游戏中昵称")
+    gameid = models.CharField(max_length=20, verbose_name="游戏ID")
+
+    class Meta:
+        verbose_name = "游戏信息"
+        verbose_name_plural = verbose_name
 
 class Score(models.Model):
-    player = models.ForeignKey(Player, on_delete=models.CASCADE)
-    score = models.IntegerField(default=0)
-    cost = models.IntegerField(default=0)
-    is_host = models.IntegerField(default=0)
-    create_time = models.DateTimeField('create time')
-    room_id = models.CharField(max_length=20)
-    refresh_time = models.DateTimeField('refresh time')
+    player = models.ForeignKey(Player, on_delete=models.CASCADE, verbose_name="玩家")
+    score = models.IntegerField(default=0, verbose_name="分数")
+    cost = models.IntegerField(default=0, verbose_name="消费")
+    is_host = models.IntegerField(default=0, choices=IS_HOST, verbose_name="是否是房主")
+    create_time = models.DateTimeField(verbose_name="创建时间")
+    room_id = models.CharField(max_length=20, verbose_name="房间ID")
+    refresh_time = models.DateTimeField(verbose_name="最近更新时间")
+
+    class Meta:
+        verbose_name = "得分情况"
+        verbose_name_plural = verbose_name
 
 class HistoryGame(models.Model):
-    club = models.ForeignKey(Clubs, on_delete=models.CASCADE)
-    room_id = models.CharField(max_length=10)
-    hoster_name = models.CharField(max_length=20)
-    hoster_id = models.IntegerField(default=0)
-    round_number = models.IntegerField(default=0)
-    start_time = models.CharField(max_length=20)
-    player_data = models.CharField(max_length=1000,default='none')#playerResult.playerData的数组
-    create_time = models.DateTimeField('create time')
-    cost = models.IntegerField(default=0)
-    score = models.IntegerField(default=0)
-    refresh_time = models.DateTimeField('refresh time')
+    club = models.ForeignKey(Clubs, on_delete=models.CASCADE, verbose_name="俱乐部")
+    room_id = models.CharField(max_length=10, verbose_name="房间号")
+    hoster_name = models.CharField(max_length=20, verbose_name="主机名称")
+    hoster_id = models.IntegerField(default=0, verbose_name="主机ID")
+    round_number = models.IntegerField(default=0, verbose_name="不知道？round_number")
+    start_time = models.CharField(max_length=20, verbose_name="游戏开始时间")
+    player_data = models.CharField(max_length=1000,default='none', verbose_name="游戏数据")#playerResult.playerData的数组
+    create_time = models.DateTimeField(verbose_name="创建时间")
+    cost = models.IntegerField(default=0, verbose_name="消费")
+    score = models.IntegerField(default=0, verbose_name="得分")
+    refresh_time = models.DateTimeField(verbose_name="最近刷新时间")
 
     def __str__(self):
         return "%s_%s" % (self.room_id, self.hoster_name)
+
+    class Meta:
+        verbose_name = "游戏历史记录"
+        verbose_name_plural = verbose_name
 
 class HistoryGameClearCost(models.Model):
     history_id = models.IntegerField(default=0)
     cost = models.IntegerField(default=0)
     create_time = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name = "游戏记录管理费清理"
+        verbose_name_plural = verbose_name
+
 class Cdkey(models.Model):
-    cdkey = models.CharField(primary_key=True, max_length=50)
-    key_type = models.IntegerField(default=0) #1 -1天，2 - 1周， 3- 1个月
-    status = models.IntegerField(default=0)
-    create_time = models.IntegerField(default=0)
+    cdkey = models.CharField(primary_key=True, max_length=50, verbose_name="密钥")
+    key_type = models.IntegerField(default=0, choices=CDKEY_TYPE, verbose_name="密钥类型") #1 -1天，2 - 1周， 3- 1个月
+    status = models.IntegerField(default=0, choices=USE_STATUS, verbose_name="使用状态")
+    create_time = models.IntegerField(default=0, verbose_name="创建时间")
+
+    class Meta:
+        verbose_name = "Cdkey"
+        verbose_name_plural = verbose_name
 
 class WrongImage(models.Model):
-    club_name = models.CharField(max_length=20)
-    image = models.CharField(max_length=100)
-    create_time = models.IntegerField(default=0)
+    club_name = models.CharField(max_length=20, verbose_name="俱乐部")
+    image = models.CharField(max_length=100, verbose_name="图片")
+    create_time = models.IntegerField(default=0, verbose_name="创建时间")
+
+    def __str__(self):
+        return self.club_name
+
+    class Meta:
+        verbose_name = "错误图片"
+        verbose_name_plural = verbose_name
 
 class ScoreChange(models.Model):
-    player = models.ForeignKey(Player, on_delete=models.CASCADE)
-    score = models.IntegerField(default=0) 
-    agent = models.CharField(max_length=500)
-    ip = models.CharField(max_length=20)
-    create_time = models.IntegerField(default=0)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE, verbose_name="玩家")
+    score = models.IntegerField(default=0, verbose_name="分数变化值")
+    agent = models.CharField(max_length=500, verbose_name="客户端")
+    ip = models.CharField(max_length=20, verbose_name="IP")
+    create_time = models.IntegerField(default=0, verbose_name="创建时间")
+
+    class Meta:
+        verbose_name = "分数变更记录"
+        verbose_name_plural = verbose_name
 
 class Manager(models.Model):
-    club = models.ForeignKey(Clubs, on_delete=models.CASCADE)
-    wechat_nick_name = models.CharField(max_length=200)
-    nick_name = models.CharField(max_length=200)
-    create_time = models.IntegerField(default=0)
+    club = models.ForeignKey(Clubs, on_delete=models.CASCADE, verbose_name="俱乐部")
+    wechat_nick_name = models.CharField(max_length=200, verbose_name="微信昵称")
+    nick_name = models.CharField(max_length=200, verbose_name="昵称")
+    create_time = models.IntegerField(default=0, verbose_name="创建时间")
+
+    class Meta:
+        verbose_name = "管理员"
+        verbose_name_plural = verbose_name
 
