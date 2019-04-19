a6da428540a4d777b7f4648693eea4bd14e5c293
York <987835616@qq.com>
2019-04-20 00:49:14 +0800

    del nouse basedir

--- wechat_server/dealMsg/settings.py
+++ wechat_server/dealMsg/settings.py
@@ -13,10 +13,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os, time
 from DServer.settings import *
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
