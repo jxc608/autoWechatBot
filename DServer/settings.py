29eeb8f6eadbb170d0178feb2c9d784ba57fbe0e
York <987835616@qq.com>
2019-04-20 10:42:13 +0800

    settings

--- DServer/settings.py
+++ DServer/settings.py
@@ -57,14 +57,14 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
+    # 'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'uadetector.django.middleware.UADetectorMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    # 'uadetector.django.middleware.UADetectorMiddleware',
 ]
 
 ROOT_URLCONF = 'DServer.urls'
